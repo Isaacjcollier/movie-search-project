e6ffd17dd321eeacbaa27578e33572d72db1d007
Isaacjcollier <isaacjcollier@gmail.com>
2016-07-26 13:03:20 -0600

    halfway

--- src/main.js
+++ src/main.js
@@ -12,8 +12,12 @@ $(function() {
     }).done(function (results) {
       console.log(results);
       var image = results.Poster
-      $('#posterImage').attr('src', image)
+      $('#posterImage').append('<div><img class="poster" width="300" height="400" src="' + image +'"> <h1>' +results.Title + '</h></div>')
       console.log(image);
+      // $('#posterImage > .poster').append('<h1>' +results.Title + '</h1>')
+      for (var i = 0; i < results.Genre.length; i++) {
+        $('#genreList').append(results.Genre[i])
+      }
     })
   })
 })
